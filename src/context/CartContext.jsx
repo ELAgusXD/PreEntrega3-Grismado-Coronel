e5ec33318dbd75060fa7edf56436139e035a1965
Ulises Grismado <94875813+ELAgusXD@users.noreply.github.com>
2023-11-29 23:15:59 -0300

    Update CartContext.jsx

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,51 +1,40 @@
 import React, {createContext, useState, useContext} from 'react';
 
 export const CartContext = createContext();
-
 export const useCartContext = () => {
 useContext(CartContext)
 }
 
 export const CartProvider = ({children}) =>{
 
-
     const [cartItems, setCartItems] = useState([])
     const [cartCount, setCartCount] = useState(0)
-
-
     const addItem = (item, amount) =>{
-    
         if (isInCart(item.id)){
-                const newCart = [...cartItems]
+            const newCart = [...cartItems]
                 for (const itemCompare of newCart) {
                     if (itemCompare.item.id === item.id) {
                         itemCompare.amount = itemCompare.amount + amount
                     }
                 }
                 setCartItems(newCart)
-    
             } else {
                 setCartItems([...cartItems, { item: item, amount: amount }])
             }
         }
-
-
     const modifyCartQuantity=(amount) => {
         setCartCount(amount);
     }
-
+    
     const cartCounter=() =>{
         let total = 0;
         cartItems.forEach((pcount) => (total += pcount.amount));
-    
         return total;
       }
 
     const removeItem = (itemId) => {
-
         let newCart = cartItems.filter(produc=> produc.item.id !== itemId)
         setCartItems(newCart)
-
     }
 
     const clear = () => {
